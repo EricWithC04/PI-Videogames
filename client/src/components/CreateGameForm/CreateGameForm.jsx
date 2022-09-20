c2713a0f6439cc7cbf5816a88ead32526b77d3ad
EricWithC04 <negativegame@gmail.com>
2022-09-20 17:06:17 -0300

    expresiones regulares y errores

--- client/src/components/CreateGameForm/CreateGameForm.jsx
+++ client/src/components/CreateGameForm/CreateGameForm.jsx
@@ -14,6 +14,10 @@ const CreateGameForm = () => {
     const allGenres = useSelector(state => state.allGenres)
     const allPlatforms = useSelector(state => state.allPlatforms)
 
+    const regEx = {
+        img: /^(ftp|http|https):\/\/[^ "]+$/,
+        date: /^(0[1-9]|1\d|2\d|3[01])\/(0[1-9]|1[0-2])\/(1[0-9][0-9][0-9]|2[0-9][0-9][0-9])$/
+    }
     const [ errors, setErrors] = useState({})
     const [ input, setInput ] = useState({
         name: "",
@@ -29,9 +33,13 @@ const CreateGameForm = () => {
         let errors = {};
         if (!input.name) errors.name = "El videojuego necesita un nombre!"
         else if (!input.img) errors.img = "Debes añadir una imagen del juego"
+        else if (!regEx.img.test(input.img)) errors.img = "Añade una URL valida"
         else if (!input.description) errors.description = "Debes añadir una descripción"
         else if (!input.release) errors.release = "Debes añadir una fecha de publicación"
+        else if (!regEx.date.test(input.release)) errors.release = "Debes añadir una fecha valida"
         else if (!input.rating) errors.rating = "Debes añadir el rating"
+        else if (input.rating < 1) errors.rating = "El rating debe ser mayor a 1"
+        else if (input.rating > 5) errors.rating = "El rating debe ser menor a 5"
         else if (!input.genres.length) errors.genres = "Debes añadir al menos un genero"
         else if (!input.platforms.length) errors.platforms = "Debes añadir al menos una plataforma"
         return errors
@@ -56,15 +64,49 @@ const CreateGameForm = () => {
     return (
         <form onSubmit={(e) => handleSubmit(e)}>
             <label>Nombre</label>
-            <input name='name' onChange={(e) => handleChange(e)} type="text" value={input.name}/>
+            <input 
+                name='name' 
+                onChange={(e) => handleChange(e)} 
+                type="text" 
+                value={input.name}
+                placeholder="nombre del juego..."
+            />
+            {errors.hasOwnProperty("name") ? (<p>{errors.name}</p>) : null}
             <label>Imagen</label>
-            <input name='img' onChange={(e) => handleChange(e)} type="text" value={input.img}/>
+            <input 
+                name='img' 
+                onChange={(e) => handleChange(e)} 
+                type="text" 
+                value={input.img}
+                placeholder="https://imagen..."
+            />
+            {errors.hasOwnProperty("img") ? (<p>{errors.img}</p>) : null}
             <label>Descripción</label>
-            <input name='description' onChange={(e) => handleChange(e)} type="text" value={input.description}/>
+            <input 
+                name='description' 
+                onChange={(e) => handleChange(e)} 
+                type="text" 
+                value={input.description}
+                placeholder="descripción..."
+            />
+            {errors.hasOwnProperty("description") ? (<p>{errors.description}</p>) : null}
             <label>Fecha de Lanzamiento</label>
-            <input name='release' onChange={(e) => handleChange(e)} type="text" value={input.release}/>
+            <input 
+                name='release' 
+                onChange={(e) => handleChange(e)} 
+                type="text" 
+                value={input.release}
+                placeholder="dd/mm/year"
+            />
+            {errors.hasOwnProperty("release") ? (<p>{errors.release}</p>) : null}
             <label>Rating</label>
-            <input name='rating' onChange={(e) => handleChange(e)} type="number" value={input.rating}/>
+            <input 
+                name='rating' 
+                onChange={(e) => handleChange(e)} 
+                type="number" 
+                value={input.rating}
+            />
+            {errors.hasOwnProperty("rating") ? (<p>{errors.rating}</p>) : null}
             <select>
                 <option value="">Elegir Genero</option>
                 {
@@ -76,6 +118,7 @@ const CreateGameForm = () => {
                 }
             </select>
             <input type="text" />
+            {errors.hasOwnProperty("genres") ? (<p>{errors.genres}</p>) : null}
             <select>
                 <option value="">Elegir Plataformas</option>
                 {
@@ -87,6 +130,7 @@ const CreateGameForm = () => {
                 }
             </select>
             <input type="text" />
+            {errors.hasOwnProperty("platforms") ? (<p>{errors.platforms}</p>) : null}
             <button type="submit">Crear Videojuego</button>
         </form>
     );
