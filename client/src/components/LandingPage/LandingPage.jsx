43e3ec544ac9bae89f80152f3074ed5045586093
EricWithC04 <negativegame@gmail.com>
2022-09-23 20:27:46 -0300

    first styles

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from "./LandingPage.module.css";
 
 const LandingPage = () => {
     return (
-        <div>
-            <Link to="/Home">
-                <button>Ingresar</button>
-            </Link>
+        <div className={styles.container}>
+            <div className={styles.welcome}>
+                <h1>Bienvenido a mi PI-Videogames</h1>
+                <p className={styles.text}>
+                    Hola, soy Eric y este es mi PI de Videojuegos.
+                    Esta es una aplicación desarrollada durante el bootcamp de Soy Henry
+                    como PI (Proyecto Individual) para demostrar las habilidades desarrolladas
+                    durante el mismo. Para acceder a la pagina principal, presiona el siguiente boton
+                </p>
+                <Link to="/Home">
+                    <button className={styles.btn}>Ingresar</button>
+                </Link>
+            </div>
         </div>
     );
 };
