c8a1f7a5db0fcf2040a12b1660dd578b918e29d2
EricWithC04 <negativegame117@gmail.com>
2023-02-24 14:39:38 -0300

    ultimos detallitos a about me

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import NavBar from '../NavBar/NavBar';
+import foto from "../../img/fotoYo.jpg"
 import styles from "./About.module.css";
 
 const About = () => {
@@ -9,13 +9,15 @@ const About = () => {
             <NavBar />
             <div className={styles.aboutContainer}>
                 <div className={styles.name}>
+                    <img src={foto} alt="foto" />
                     <h1>Eric A. Mercado</h1>
                 </div>
                 <div className={styles.text}>
                     <p>
-                        Soy Fullstack Developer Jr, empecé en la programación a los 13 años, 
-                        soy apasionado por la tecnología y me encanta participar 
-                        en proyectos desafiantes e innovadores.
+                        Soy Fullstack Developer Jr, empecé en la programación cuando estaba en la secundaria, 
+                        soy apasionado por la tecnología y el desarrollo.
+                        Me empecé a interesar en la programación cuando tenía 13 años, en aquellos momentos
+                        me interesaba bastante el desarrollo de videojuegos y tuve mis primeras experiencias programando
                         Me capacite principalmente en el Bootcamp de Soy Henry, en el cual desarrollé
                         esta aplicación como un Proyecto Final. Las tecnologías utilizadas fueron HTML,
                         CSS Modules, JavaScript, React, Redux, Node.js, Express y PostgreSQL. 
